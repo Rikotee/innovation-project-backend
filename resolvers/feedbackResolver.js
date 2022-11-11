c764aa7e034ba2483458ffe9bbf6ac671057edd9
Rikotee <teerek81@gmail.com>
2022-11-11 16:38:36 +0200

    feedback list

--- resolvers/feedbackResolver.js
+++ resolvers/feedbackResolver.js
@@ -8,6 +8,23 @@ export default {
       }
         return await Feedback.findById(args.id);
       },
+
+
+          // find list
+    feedbacks: async (parent, args, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('Not authorised');
+      }
+      const start = args.start || 0;
+      const limit = args.limit || 10;
+      const feedbacks = Feedback.find().skip(start).limit(limit);
+
+      return args.bounds
+        ? feedbacks
+            .find()
+        : feedbacks;
+    },
+
   },
   Mutation: {
     addFeedback: async (parent, args, context) => {
